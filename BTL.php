b775e482a053312b4dff4c275b314094f92d3d5c
huynq721 <huynq721@wru.vn>
2019-10-11 00:55:40 +0700

    first commit

---
+++ BTL.php
@@ -0,0 +1,273 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Trường Đại Học Nguyễn Tất Thành</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+	<link href="style2.css" rel="stylesheet">
+	<script src="https://kit.fontawesome.com/aef1236d55.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+	<div id="wrapper">
+		<header>
+			<div id="top-header">
+				<div class=" contact">
+					<span>Hotline:  0902.298.300 - 0906.298.300</span>
+				</div>
+				<div class="sub-menu">
+					<a href="http://ctsv.ntt.edu.vn">SINH VIÊN	 </a>
+					<a href="http://tcns.ntt.edu.vn">GIẢNG VIÊN	 </a>
+					<a href="http://niie.edu.vn">ĐÀO TẠO QUỐC TẾ	 </a>
+				</div>
+				<div class="search">
+					<form action="/web/tim-kiem">
+					<div class="input-search">
+						<input type="search" name="keyword" id="search" placeholder="Tìm kiếm thông tin">
+						<span>
+							<button class="btn btn-transparent btn-secondary border-0">
+								<i class="fa fa-search" aria-hidden="true">
+								</i>
+							</button>
+							<img src="./images/ico-flagen.jpg" alt="">
+						</span>
+						</div>
+					</form>
+				</div>
+				</div>
+			<div id="main-header">
+				<div class="logo">
+					<img alt="" src="http://ntt.edu.vn/web/frontend/images/logo_ntt.png" />
+				</div>
+					<div class="main-menu">
+						<a href=""><i class="fas fa-info-circle"></i>GIỚI THIỆU	</a>
+						<a href=""><i class="fas fa-graduation-cap"></i>TUYỂN SINH 	</a>
+						<a href=""><i class="fas fa-book"></i>ĐÀO TẠO 		</a>
+						<a href=""><i class="fas fa-bookmark"></i>NGHIÊN CỨU 	</a>
+						<a href=""><i class="far fa-handshake"></i>HỢP TÁC DOANG NGHIỆP 	</a>
+					</div>
+				</div>
+			</div>
+		</header>
+	</div>	
+	<div class="container-fluid">
+		<div class="row">
+			<section id="content">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="carousel slide" id="carousel-766240">
+							<ol class="carousel-indicators">
+								<li data-slide-to="0" data-target="#carousel-766240">
+								</li>
+								<li data-slide-to="1" data-target="#carousel-766240" class="active">
+								</li>
+								<li data-slide-to="2" data-target="#carousel-766240">
+								</li>
+							</ol>
+							<div class="carousel-inner" >
+								<div class="carousel-item">
+									<img class="d-block w-100" alt="Carousel Bootstrap First" src="http://ntt.edu.vn/web/upload/images/slider/Chinh-sach-hoc-bong-NTTU_Web-Slider.jpg" />
+									<div class="carousel-caption ">
+										<h4>
+											CHÍNH SÁCH HỌC BỔNG TRƯỜNG ĐẠI HỌC NGUYỄN TẤT THÀNH 2019
+										</h4>
+										<p></p>
+									</div>
+								</div>
+								<div class="carousel-item active">
+									<img class="d-block w-100" alt="Carousel Bootstrap Second" src="http://ntt.edu.vn/web/upload/images/slider/Lien-thong-NTTU_Web-Slider.jpg" />
+									<div class="carousel-caption">
+										<h4>
+											TUYỂN SINH LIÊN THÔNG 2019
+										</h4>
+										<p></p>
+									</div>
+								</div>
+								<div class="carousel-item">
+									<img class="d-block w-100" alt="Carousel Bootstrap Third" src="http://ntt.edu.vn/web/upload/images/slider/Thac-sy-2019-NTTU_Web-Slider.jpg" />
+									<div class="carousel-caption">
+										<h4>
+											TUYỂN SINH THẠC SĨ ĐỢT 1 NĂM 2019 TẠI ĐH NGUYỄN TẤT THÀNH
+										</h4>
+										<p></p>
+									</div>
+								</div>
+							</div>
+							<a class="carousel-control-prev" href="#carousel-766240" data-slide="prev">
+								<span class="carousel-control-prev-icon">
+
+								</span>
+								<span class="sr-only">Previous</span>
+								</a> 
+								<a class="carousel-control-next" href="#carousel-766240" data-slide="next">
+									<span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+						</div>
+					</div>
+				</div>
+				<div class="d-md-flex section mt-4">
+					<div class="col-md-9">
+							<h3 class="heading-2">
+								<a href="">Tin tức</a>
+							</h3>
+							<div class="row">
+								<div class="col-md-4">
+										<div class="card">
+										<a class="thumbnail" href="http://ntt.edu.vn/web/tin-tuc/dh-nguyen-tat-thanh-vinh-danh-cac-cu-nhan-dieu-duong-tai-le-tot-nghiep-nam-2019">
+											<img class="card-img-top" alt="" src="./images/TIn tuc/9.1.jpg" />
+											</a>
+											<div class="card-block">
+											<a class="title" href="http://ntt.edu.vn/web/tin-tuc/dh-nguyen-tat-thanh-vinh-danh-cac-cu-nhan-dieu-duong-tai-le-tot-nghiep-nam-2019">
+												<h5 class="card-title">
+												ĐH Nguyễn Tất Thành vinh danh các cử nhân Điều dưỡng tại Lễ tốt nghiệp năm 2019
+												</h5>
+												</a>
+												<p class="card-text">
+												ĐH Nguyễn Tất Thành vinh danh các cử nhân Điều dưỡng tại Lễ tốt nghiệp năm 2019
+													</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4">
+										<div class="card">
+										<a class="thumbnail" href="http://ntt.edu.vn/web/tin-tuc/nu-duoc-si-tuong-lai-co-bap-cuon-cuon-vua-thang-cuoc-thi-the-hinh">
+											<img class="card-img-top" alt="" src="./images/TIn tuc/10.jpg" />
+											</a>
+											<div class="card-block">
+											<a class="title" href="http://ntt.edu.vn/web/tin-tuc/nu-duoc-si-tuong-lai-co-bap-cuon-cuon-vua-thang-cuoc-thi-the-hinh">
+												<h5 class="card-title">
+												Nữ dược sĩ tương lai cơ bắp cuồn cuộn, vừa thắng cuộc thi thể hình
+												</h5>
+												</a>
+												<p class="card-text">
+													NTTU - Trần Xuân Quỳnh - sinh viên khoa Dược, ĐH Nguyễn Tất Thành, TP.HCM - khiến nhiều người chú ý với hình thể săn chắc, đặc biệt là cơ bụng 6 múi. 9X có số đo 3 vòng là 84 - 65 - 96.
+													</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-md-4">
+										<div class="card">
+										<a class="thumbnail" href="http://ntt.edu.vn/web/tin-tuc/dh-nguyen-tat-thanh-tuyen-sinh-lien-thong-cac-nganh-dao-tao">
+											<img class="card-img-top" alt="" src="./images/TIn tuc/11.png" />
+											</a>
+											<div class="card-block">
+											<a class="title" href="http://ntt.edu.vn/web/tin-tuc/dh-nguyen-tat-thanh-tuyen-sinh-lien-thong-cac-nganh-dao-tao">
+												<h5 class="card-title">
+												ĐH Nguyễn Tất Thành tuyển sinh liên thông các ngành đào tạo
+												</h5>
+												</a>
+												<p class="card-text">
+												NTTU - ĐH Nguyễn Tất Thành tuyển sinh liên thông các ngành đào tạo...
+													</p>
+											</div>
+										</div>
+									</div>
+								</div>
+							<ul class="list-unstyled" style="float: right;">
+								<li class="c-blue-a5 font-weight-bold">
+									<a href="http://ntt.edu.vn/web/tin-tuc/"><i class="fas fa-chevron-circle-right"></i>Xem thêm</a>
+								</li>
+							</ul>
+						</div>
+					<div class="col-md-3">
+						<h3 class="heading-2">
+							<a href="">Media</a>
+						</h3>
+						<div class="card-video" style="width: inherit">
+							<iframe class="width: inherit;" src="https://www.youtube.com/embed/AWEnDkvhJHE" frameborder="0"></iframe>
+							<div class="description">
+								<ul class="list-style-1" >
+									<li><i class="fa fa-angle-right"></i>
+									<a href="https://www.youtube.com/watch?v=JViDyojZzhs">ĐH Nguyễn Tất Thành tiến hành tổ chức kiểm định chương trình đào tạo theo tiêu chuẩn AUN-QA</a> 
+									</li>
+									<li><i class="fa fa-angle-right"></i>
+									<a href="https://www.youtube.com/watch?v=iZ1EUr2DAMs">Điểm tin tháng 8</a> 
+									</li>
+									<li><i class="fa fa-angle-right"></i>
+									<a href="https://www.youtube.com/channel/UCPuljJ0RsiXHf3odISzjqDw">ĐH Nguyễn Tất Thành – 20 năm dấu ấn một chặng đường</a> 
+									</li>
+								</ul>
+							</div>
+						</div>
+						<ul class="list-unstyled" style="float: right;">
+								<li class="c-blue-a5 font-weight-bold">
+									<a href="http://ntt.edu.vn/web/tin-tuc/"><i class="fas fa-chevron-circle-right"></i>Xem thêm</a>
+								</li>
+							</ul>
+					</div>				
+				</div>
+				<div class="section sub-slider">
+				</div>
+				<div class="section d-md-flex border-top pt-4">
+					<div class="col-md-8">
+						<div class="row">
+						<article class="col-md-6 post-home">
+								<h2 class="heading-2"><a href="http://ntt.edu.vn/web/hop-tac-quoc-te"> Hợp Tác Quốc Tế </a>
+								</h2><hr width="10%" align="left">
+								<div class="card">
+									<a class="thumbnail large" href=""><img class="card-img-top"  src="./images/TIn tuc/11.jpg" /></a>
+									<div class="card-block">
+										<a class="title" style="overflow: hidden; height: 65px;" href="">
+											<h5 class="card-title">
+											Sinh viên Hàn Quốc tham gia học tập Tiếng Việt ngắn hạn tại ĐH Nguyễn Tất Thành
+											</h5></a>
+										<p class="card-text">
+										NTTU – Sáng ngày 07/10/2019, tại cơ sở quận 7, Trường ĐH Nguyễn Tất Thành đã tổ chức thành công chương trình Khai giảng khóa học Tiếng Việt ĐH Kyungsung (Hàn Quốc)</p>
+									</div>
+								</div>
+								<ul class="list-unstyled" style="float: left;">
+								<li class="c-blue-a5 font-weight-bold">
+									<a href="http://ntt.edu.vn/web/tin-tuc/"><i class="fas fa-chevron-circle-right"></i>Xem thêm</a>
+								</li>
+							</ul>
+						</article>
+							<article class="col-md-6 post-home">
+								<h2 class="heading-2"><a href="">Môi trường học tập</a></h2><hr  align="left" width="20%" style="font-weight: bold">
+								<div class="card">
+								<a class="thumbmail large" href="">
+									<img class="card-img-top"src="./images/TIn tuc/12.jpg" /></a>
+									<div class="card-block">
+									<a class="title" style="overflow: hidden; height: 65px;" href="">
+										<h5 class="card-title">
+										Tìm hiểu về các câu lạc bộ tại Trường ĐH Nguyễn Tất Thành
+										</h5></a>
+										<p class="card-text">
+										NTTU - Những năm gần đây, bên cạnh việc nâng cao chất lượng đào tạo, Trường ĐH Nguyễn Tất Thành không ngừng phát triển mô hình câu lạc bộ sinh viên nhằm tạo môi trường học tập và vui chơi tốt nhất cho người học. Thông qua mô hình CLB, các bạn sinh viên có thể dễ dàng trao đổi kiến thức, kinh nghiệm ngoài giờ học. Các CLB là nơi bạn thư giãn sau những giờ học căng thẳng, thể hiện niềm yêu thích, đam mê và góp công sức vào các chương trình tình nguyện vì cộng đồng. Mỗi CLB mang một màu sắc khác nhau bổ trợ rất nhiều cho các bạn có nhu cầu học ngoài sách vở, giao lưu giúp đỡ lẫn nhau như cùng nhau học tập kinh nghiệm từ các thế hệ anh chị đi trước .</p>
+									</div>
+								</div>
+								<ul class="list-unstyled" style="float: left;">
+								<li class="c-blue-a5 font-weight-bold">
+									<a href="http://ntt.edu.vn/web/tin-tuc/"><i class="fas fa-chevron-circle-right"></i>Xem thêm</a>
+								</li>
+							</ul>
+							</article>
+						</div>
+					</div>
+					<div class="col-md-4">
+					<h2 class="heading-2"><a href="">Sự kiện nổi bật</a></h2>
+						<ul class="list-unstyled">
+							<li class="media event">
+								<div class="d-flex align-items-center mr-3 thumb c-white">
+									<span class="inner text-center w-200" style="background-color:red"
+									>
+										Oct
+										<br>
+										<b>10</b>
+									</span>
+								</div>
+							</li>	
+						</ul>
+
+					</div>
+				</div>
+			</section>
+		</div>
+	</div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
